936d9554f4c916803198f0687436eb7454618b3b
vvvadim <v.vladimirov@it-park.tech>
2025-05-17 12:31:00 +0300

    add hashing user password

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,15 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        # Adding the below line made it work for me.
+        instance.is_active = True
+        if password is not None:
+            # Set password does the hash, so you don't need to call make_password
+            instance.set_password(password)
+        instance.save()
+        return instance
