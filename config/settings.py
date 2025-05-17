6a8d885180dc963570d560a45fa8cb808c96ba4a
vvvadim <v.vladimirov@it-park.tech>
2025-05-17 12:27:44 +0300

    add corsheaders.middleware.CorsMiddleware in settings.py

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
